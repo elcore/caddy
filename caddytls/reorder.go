eea4e0eeec7497482ee9a7498ca9bc08957a7cf4
Eldin Hadzic <eldinhadzic@protonmail.com>
2016-12-28 14:22:15 +0000

    Modify TLS Config based on ClientHelloInfo

---
+++ caddytls/reorder.go
@@ -0,0 +1,62 @@
+package caddytls
+
+import "crypto/tls"
+
+// isChaCha returns true if the provided cipher suite represents ChaCha20 cipher suites
+func isChaCha(suiteID uint16) bool {
+	switch suiteID {
+	case 0xcc13, 0xcc14, 0xcca8, 0xcca9, 0xccac:
+		return true
+	default:
+		return false
+	}
+}
+
+// isGREASE returns true if the provided cipher suite represents GREASE for TLS
+func isGREASE(suiteID uint16) bool {
+	switch suiteID {
+	case 0x0A0A, 0x1A1A, 0x2A2A, 0x3A3A, 0x4A4A, 0x5A5A, 0x6A6A, 0x7A7A, 0x8A8A, 0x9A9A, 0xAAAA, 0xBABA, 0xCACA, 0xDADA, 0xEAEA, 0xFAFA:
+		return true
+	default:
+		return false
+	}
+}
+
+// PreferChaChaIfFirst moves ChaCha20 cipher suites to the front
+func (c *Config) PreferChaChaIfFirst(clientHello *tls.ClientHelloInfo) (*tls.Config, error) {
+	if isGREASE(clientHello.CipherSuites[0]) && !isChaCha(clientHello.CipherSuites[1]) {
+		// Return unmodified tls.Config
+		return nil, nil
+	} else if !isGREASE(clientHello.CipherSuites[0]) && !isChaCha(clientHello.CipherSuites[0]) {
+		// Return unmodified tls.Config
+		return nil, nil
+	}
+
+	ciphers := c.TLSConfig.CipherSuites
+
+	reordered := make([]uint16, 0, len(ciphers))
+	// first pass: preferred ciphers
+	for _, suiteID := range ciphers {
+		if isChaCha(suiteID) {
+			reordered = append(reordered, suiteID)
+		}
+	}
+	// second pass: all remaining
+	for _, suiteID := range ciphers {
+		if !isChaCha(suiteID) {
+			reordered = append(reordered, suiteID)
+		}
+	}
+
+	c.TLSConfig.CipherSuites = reordered
+
+	// It's safe to set the maximum version as the minimum version here
+	if c.TLSConfig.MaxVersion > c.TLSConfig.MinVersion {
+		c.TLSConfig.MinVersion = c.TLSConfig.MaxVersion
+	}
+
+	c.ModifiedTLSConfig = true
+
+	// Return modified copy of tls.Config
+	return c.TLSConfig, nil
+}
---
+++ caddytls/reorder_test.go
@@ -0,0 +1,146 @@
+package caddytls
+
+import (
+	"crypto/tls"
+	"reflect"
+	"testing"
+)
+
+var (
+	config = &Config{
+		TLSConfig: &tls.Config{
+			CipherSuites: []uint16{
+				0xc02c,
+				0xc030,
+				0xc02b,
+				0xc02f,
+				0xcca9,
+				0xcca8,
+			},
+			MinVersion: 0x0302,
+			MaxVersion: 0x0303,
+		},
+	}
+
+	expectedTLSConfig = &tls.Config{
+		CipherSuites: []uint16{
+			0xcca9,
+			0xcca8,
+			0xc02c,
+			0xc030,
+			0xc02b,
+			0xc02f,
+		},
+		MinVersion: 0x0303,
+		MaxVersion: 0x0303,
+	}
+)
+
+func TestPreferChaChaIfFirstWithGREASE(t *testing.T) {
+	c := *config
+
+	clientHello := &tls.ClientHelloInfo{
+		CipherSuites: []uint16{
+			0x0A0A,
+			0xcca9,
+			0xcca8,
+			0xc02c,
+			0xc030,
+			0xc02b,
+			0xc02f,
+		},
+	}
+
+	output, err := c.PreferChaChaIfFirst(clientHello)
+	if err != nil {
+		t.Fatalf("Did not expect an error, but got %v", err)
+	}
+	if output == nil {
+		t.Fatalf("Expected a modified tls.Config")
+	}
+
+	if !reflect.DeepEqual(output, expectedTLSConfig) {
+		for i, actual := range output.CipherSuites {
+			if actual != expectedTLSConfig.CipherSuites[i] {
+				t.Errorf("Expected ciphers in position %d to be %v, got %v", i, expectedTLSConfig.CipherSuites[i], actual)
+			}
+		}
+		if got, want := output.MinVersion, expectedTLSConfig.MinVersion; got != want {
+			t.Errorf("Expected min version to be %x, got %x", want, got)
+		}
+		if got, want := output.MaxVersion, expectedTLSConfig.MaxVersion; got != want {
+			t.Errorf("Expected max version to be %x, got %x", want, got)
+		}
+	}
+
+	if !c.ModifiedTLSConfig {
+		t.Fatal("config.ModifiedTLSConfig should be true")
+	}
+}
+
+func TestPreferChaChaIfFirstWithoutGREASE(t *testing.T) {
+	c := *config
+
+	clientHello := &tls.ClientHelloInfo{
+		CipherSuites: []uint16{
+			0xcca9,
+			0xcca8,
+			0xc02c,
+			0xc030,
+			0xc02b,
+			0xc02f,
+		},
+	}
+
+	output, err := c.PreferChaChaIfFirst(clientHello)
+	if err != nil {
+		t.Fatalf("Did not expect an error, but got %v", err)
+	}
+	if output == nil {
+		t.Fatalf("Expected a modified tls.Config")
+	}
+
+	if !reflect.DeepEqual(output, expectedTLSConfig) {
+		for i, actual := range output.CipherSuites {
+			if actual != expectedTLSConfig.CipherSuites[i] {
+				t.Errorf("Expected ciphers in position %d to be %v, got %v", i, expectedTLSConfig.CipherSuites[i], actual)
+			}
+		}
+		if got, want := output.MinVersion, expectedTLSConfig.MinVersion; got != want {
+			t.Errorf("Expected min version to be %x, got %x", want, got)
+		}
+		if got, want := output.MaxVersion, expectedTLSConfig.MaxVersion; got != want {
+			t.Errorf("Expected max version to be %x, got %x", want, got)
+		}
+	}
+
+	if !c.ModifiedTLSConfig {
+		t.Fatal("config.ModifiedTLSConfig should be true")
+	}
+}
+
+func TestPreferChaChaIfFirstWithoutChaCha(t *testing.T) {
+	c := *config
+
+	clientHello := &tls.ClientHelloInfo{
+		CipherSuites: []uint16{
+			0x0A0A,
+			0xc02c,
+			0xc030,
+			0xc02b,
+			0xc02f,
+		},
+	}
+
+	output, err := c.PreferChaChaIfFirst(clientHello)
+	if err != nil {
+		t.Fatalf("Did not expect an error, but got %v", err)
+	}
+	if output != nil {
+		t.Fatalf("Expected an unmodified tls.Config")
+	}
+
+	if c.ModifiedTLSConfig {
+		t.Fatal("config.ModifiedTLSConfig should be false")
+	}
+}
